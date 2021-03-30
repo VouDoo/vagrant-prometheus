3f561be88175c3f98cfd156a0cba3795ad5756b1
Maxence <maxgrymonprez@live.fr>
2021-03-30 23:45:59 +0100

    Update install-grafana.sh

--- scripts/install-grafana.sh
+++ scripts/install-grafana.sh
@@ -1,6 +1,7 @@
 version=7.4.5-1
 
 yum install -y https://dl.grafana.com/oss/release/grafana-${version}.x86_64.rpm
+
 systemctl daemon-reload
 systemctl enable grafana-server
 systemctl start grafana-server
